32277d797e3ecb4a7e253815173b1a482e28a214
RipleTV <gryshyn2000@gmail.com>
2022-01-07 13:14:29 +0200

    second

--- lesson3.sol
+++ lesson3.sol
@@ -1,57 +1,49 @@
 pragma solidity ^0.4.18;
 
-contract IntAndUint{
-	int256 a = -45;
-	uint b = 150;
+contract keyWords{
+uint public oneWei = 1 wei;
+uint public oneSzabo = 1 szabo;
+uint public oneFinney = 1 finney;
+uint public oneEther = 1 ether;
 
-	function plus(int256 x) public view returns(int256){
-		return a + x;
-	}
+// время
 
-	function plus_u(uint8 x) public view returns(uint8){
-		b + x;
-	}
+uint public oneSecond = 1 seconds;
+uint public oneMinute = 1 minutes;
+uint public oneHours = 1 hours;
+uint public oneWeek = 1 weeks;
+uint public oneYear = 1 years;
 
-	function minus(int256 x) public view returns(int256){
-		return a - x;
-	}
+// блок
 
-	function minus_u(uint8 x) public view returns(uint8){
-		b - x;
-	}
+bytes32 public blockHash;
+address public miner;
+uint public difficulty;
+uint public gasLimit;
+uint public time;
+uint public time2;
 
-	function mul(int256 x) public view returns(int256){
-		return a * x;
-	}
+//  транзакции
 
-	function mul_u(uint8 x) public view returns(uint8){
-		b * x;
-	}
+bytes public data;
+uint public gas;
+address public sender;
+bytes4 public signature;
+uint public value;
 
-	function dev(int256 x) public view returns(int256){
-		return a / x;
-	}
+function Trx() public{
+    blockHash = block.blockhash(0);
+    miner = msg.sender;
+    difficulty = block.difficulty;
+    gasLimit = block.gaslimit;
+    time = block.timestamp;
+    time2 = now;
 
-	function dev_u(uint8 x) public view returns(uint8){
-		b / x;
-	}
-
-	function mod(int256 x) public view returns(int256){
-		return a % x;
-	}
-
-	function mod_u(uint8 x) public view returns(uint8){
-		b % x;
-	}
-	
-	function setA(int256 _a) public {
-		a = _a;
-	}
-	function setB(uint8 _b) public {
-		b = _b;
-	}
-
-
-
+    data = msg.data;
+    gas = msg.gas;
+    sender = msg.sender;
+    signature = msg.sig;
+    value = msg.value;
+}
 
 }
--- lesson5.sol
+++ lesson5.sol
@@ -1,49 +1,48 @@
 pragma solidity ^0.4.18;
 
-contract keyWords{
-uint public oneWei = 1 wei;
-uint public oneSzabo = 1 szabo;
-uint public oneFinney = 1 finney;
-uint public oneEther = 1 ether;
+contract lesson7pres{
 
-// время
+    function func(){
 
-uint public oneSecond = 1 seconds;
-uint public oneMinute = 1 minutes;
-uint public oneHours = 1 hours;
-uint public oneWeek = 1 weeks;
-uint public oneYear = 1 years;
+        require(msg.value > 0, "you sent 0 eth");
+        ...
+        ....
+        ...
+        ..
+        assert(msh.value > 0);
 
-// блок
-
-bytes32 public blockHash;
-address public miner;
-uint public difficulty;
-uint public gasLimit;
-uint public time;
-uint public time2;
-
-//  транзакции
-
-bytes public data;
-uint public gas;
-address public sender;
-bytes4 public signature;
-uint public value;
-
-function Trx() public{
-    blockHash = block.blockhash(0);
-    miner = msg.sender;
-    difficulty = block.difficulty;
-    gasLimit = block.gaslimit;
-    time = block.timestamp;
-    time2 = now;
-
-    data = msg.data;
-    gas = msg.gas;
-    sender = msg.sender;
-    signature = msg.sig;
-    value = msg.value;
+        if(...){
+            ..
+            ..
+            .
+            if(...){
+                } else {
+                    revert();
+                }
+            }
+        }
+    }
 }
 
+contract lesson7{ 
+
+    function sendHalf(address _addr) public payable{
+        require(msg.value%2 == 0);
+        uint balanceBeforeTransfer = address(this).balance;
+        _addr.transfer(msg.value/2);
+
+        assert(address(this).balance == balanceBeforeTransfer - msg.value/2);
+    }
+
+        function sendHalfToOwner(address _addr) public payable{
+            uint balanceBeforeTransfer = address(this).balance;
+            owner.transfer(msg.value/2);
+
+        assert(address(this).balance == balanceBeforeTransfer - msg.value/2);
+        }
+        
+        modifier onlyEvenNumber(){
+            require(msg.value%2 == 0);
+            _;
+        }
 }
